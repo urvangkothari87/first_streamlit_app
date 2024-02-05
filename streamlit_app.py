d79a373921d53414f0e13625d1e648ad8885d088
urvangkothari87 <134337811+urvangkothari87@users.noreply.github.com>
2024-02-04 19:05:34 -0600

    Update streamlit_app.py
    added fruits_selected and fruits_to_show variables

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,16 +7,18 @@ s.header('Breakfast Menu')
 s.text('Omega 3 & Blueberry Oatmeal')
 s.text('Banana, Mango, and Spinach Smoothie')
 s.text('Hard-Boiled Free-Range Eggs')
-s.text('Avacado Toast')
+s.text('Avocado Toast')
 
 s.header('Build Your Own Fruit Smoothie')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so customers can pick the fruit they want to include
-s.multiselect("Pick some fruits:", list(my_fruit_list.set_index('Fruit').index))
+fruits_selected = s.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page
-s.dataframe(my_fruit_list)
+s.dataframe(fruits_to_show)
 
 
